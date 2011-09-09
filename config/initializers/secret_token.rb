895c2db4877d914f00d18dc3c695e7555661bc62
Park <baks92@list.ru>
2011-09-09 15:20:52 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Park::Application.config.secret_token = 'cee9771c95fa12d2cacb91c84718e3852e388f591b969d36866d770d932054762a09e271aaf3032ce87e17a4677da5b0006580d0cc1d5eb2597ea6e39b6d1c11'
